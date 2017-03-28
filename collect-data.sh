d4d3df76a7ee844b8f1568c219a31bbfb837af0b
Jacob Errington <git@mail.jerrington.me>
2017-03-28 10:40:26 -0400

    more runs

--- collect-data.sh
+++ collect-data.sh
@@ -5,12 +5,12 @@ exec parallel \
     -a <(echo '3g') \
     -a <(echo '3g') \
     -a <(echo 50) \
-    -a <(echo 1.0) \
+    -a <(seq 0.50 0.50 2.0) \
     -a <(seq 0.1 0.25 5.0) \
     -a <( \
         parallel -j 1 \
-            -a <(echo 50) \
-            -a <(echo 50) \
+            -a <(echo 75 ; echo 100 ; echo 125) \
+            -a <(echo 75 ; echo 100 ; echo 125) \
             ./format-monster-2.sh
     ) \
     -a <(echo 'properties.pf') \
