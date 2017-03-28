113b5f3326df7117dacb3813f09b861cd19dc6d0
Jacob Errington <git@mail.jerrington.me>
2017-03-28 09:55:33 -0400

    adjust data collection bounds

--- collect-data.sh
+++ collect-data.sh
@@ -4,15 +4,15 @@ exec parallel \
     -j 7 \
     -a <(echo '3g') \
     -a <(echo '3g') \
-    -a <(seq 10 10 70) \
-    -a <(seq 0.25 0.25 2.0) \
-    -a <(seq 0.25 0.25 2.0) \
+    -a <(echo 50) \
+    -a <(echo 1.0) \
+    -a <(seq 0.1 0.25 5.0) \
     -a <( \
         parallel -j 1 \
-            -a <(seq 5 15 150) \
-            -a <(seq 5 15 150) \
+            -a <(echo 50) \
+            -a <(echo 50) \
             ./format-monster-2.sh
     ) \
     -a <(echo 'properties.pf') \
-    -a <(seq 0.1 0.2 1.0) \
+    -a <(echo '0.25') \
     ./wrapper.sh
