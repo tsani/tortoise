1589436302db4d9fa05fda6a3a5e3a7838041cd8
Jacob Errington <git@mail.jerrington.me>
2017-03-27 23:43:48 -0400

    improve deploy

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@
 ssh tortoise@$1 bash <<'EOF'
 set -e
 
-die() {
-    echo "$@" >&2
-    exit 1
-}
-
 source .bash_profile # so we get PRISM in our PATH
 cd tortoise
 
@@ -17,5 +12,5 @@ git reset --hard origin/master
 
 echo "building code generator"
 cabal install --only-dependencies
-cabal build >/dev/null 2>&1 || die 'cabal build failed'
+cabal build
 EOF
