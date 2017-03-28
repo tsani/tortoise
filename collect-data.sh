0f74f315b6641dfeee130abfa0523c96a9448b56
Jacob Errington <git@mail.jerrington.me>
2017-03-28 10:46:10 -0400

    oops

--- collect-data.sh
+++ collect-data.sh
@@ -7,7 +7,7 @@ exec parallel \
     -a <(echo 50) \
     -a <(echo 1.0) \
     -a <(seq 0.1 0.25 5.0) \
-    -a <(./format-monster-2.sh 100 100 ; ./format-monster-2.sh 150 150)
+    -a <(./format-monster-2.sh 100 100 ; ./format-monster-2.sh 150 150) \
     -a <(echo 'properties.pf') \
     -a <(echo '0.25') \
     ./wrapper.sh
