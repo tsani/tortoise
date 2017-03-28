687cb210600cc002da855e7d8b1e23af1a96ac61
Jacob Errington <git@mail.jerrington.me>
2017-03-27 22:00:16 -0400

    paths

--- wrapper.sh
+++ wrapper.sh
@@ -2,4 +2,4 @@
 
 # Wrapper script for the deploy script.
 
-JAVA_MEM=$1 CUDD_MEM=$2 NUM_BOTS=$3 EFFICIENCY=$4 EXPONENT=$5 ENEMIES="$6" ./cnc/deploy.sh "$7" properties.pf
+JAVA_MEM=$1 CUDD_MEM=$2 NUM_BOTS=$3 EFFICIENCY=$4 EXPONENT=$5 ENEMIES="$6" ./deploy.sh "$7" properties.pf
