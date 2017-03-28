36687e2932d471b8a4d507046b1d523e232b0ef4
Jacob Errington <git@mail.jerrington.me>
2017-03-28 01:31:50 -0400

    easy MODE

--- collect-data.sh
+++ collect-data.sh
@@ -4,13 +4,13 @@ exec parallel \
     -j 7 \
     -a <(echo '3g') \
     -a <(echo '3g') \
-    -a <(echo '200') \
+    -a <(echo '10') \
     -a <(echo '1') \
     -a <(echo '1') \
     -a <( \
         parallel -j 1 \
-            -a <(seq 5 5 100) \
-            -a <(seq 5 5 100) \
+            -a <(seq 3 3 50) \
+            -a <(seq 3 3 50) \
             ./format-monster-2.sh
     ) \
     -a <(echo 'properties.pf') \
