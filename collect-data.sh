12f03056834bfc7cc7ae9993923f1e330a7303a8
Jacob Errington <git@mail.jerrington.me>
2017-03-28 02:27:12 -0400

    aparamsa

--- collect-data.sh
+++ collect-data.sh
@@ -4,15 +4,15 @@ exec parallel \
     -j 7 \
     -a <(echo '3g') \
     -a <(echo '3g') \
-    -a <(echo '10') \
-    -a <(echo '1') \
-    -a <(echo '1') \
+    -a <(seq 10 10 70) \
+    -a <(seq 0.25 0.25 2.0) \
+    -a <(seq -2.00 0.50 2.0) \
     -a <( \
         parallel -j 1 \
-            -a <(seq 3 3 50) \
-            -a <(seq 3 3 50) \
+            -a <(seq 5 15 150) \
+            -a <(seq 5 15 150) \
             ./format-monster-2.sh
     ) \
     -a <(echo 'properties.pf') \
-    -a <(echo '0.1') \
+    -a <(seq 0.1 0.2 1.0) \
     ./wrapper.sh
