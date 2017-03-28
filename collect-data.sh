c25c3b8561547573dd5b7697a26f0908cbb01914
Jacob Errington <git@mail.jerrington.me>
2017-03-28 02:28:21 -0400

    asdf

--- collect-data.sh
+++ collect-data.sh
@@ -6,7 +6,7 @@ exec parallel \
     -a <(echo '3g') \
     -a <(seq 10 10 70) \
     -a <(seq 0.25 0.25 2.0) \
-    -a <(seq -2.00 0.50 2.0) \
+    -a <(seq 0.25 0.25 2.0) \
     -a <( \
         parallel -j 1 \
             -a <(seq 5 15 150) \
